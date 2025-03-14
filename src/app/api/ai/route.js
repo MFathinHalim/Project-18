968cedbac179854a24f513a3834d85ae125fd8a2
M.Fathin Halim <halimfathin7@gmail.com>
2025-03-15 04:53:42 +0700

    fix prompt lagi

--- src/app/api/ai/route.js
+++ src/app/api/ai/route.js
@@ -16,7 +16,7 @@ export async function GET(req) {
   let prompt = searchParams.get("prompt") || "default";
   prompt = decodeBase64UrlSafe(prompt)
   const apiurl = `https://sandipbaruwal.onrender.com/gemini?prompt=${encodeURIComponent(
-"Buatkan pesan ucapan sesuai permintaan user. Gunakan bahasa yang santai, boleh pakai sedikit bahasa gen z indonesia biar terasa lebih natural, tapi tetap sopan. Tulis pesannya seperti dari seseorang yang akrab dengan penerima, tanpa harus menyebut nama langsung. Jangan pakai tanda seru, cukup titik saja. Awali dengan kalimat yang lebih sopan atau langsung ke inti pesan. Gunakan emoji kalau cocok. Berikut permintaan user: " + prompt
+"Buatkan pesan ucapan sesuai permintaan user. Gunakan bahasa yang santai, boleh pakai sedikit bahasa gen z indonesia biar terasa lebih natural, tapi tetap sopan. Tulis pesannya seperti dari seseorang yang akrab dengan penerima, tanpa harus menyebut nama langsung. Jangan pakai tanda seru, cukup titik saja. Awali dengan kalimat yang lebih sopan atau langsung ke inti pesan. Gunakan emoji kalau cocok. Jangan pake woi atau eh sama sekali. Berikut permintaan user: " + prompt
     )}`;
       
   try {
