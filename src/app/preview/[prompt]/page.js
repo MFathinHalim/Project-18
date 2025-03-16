43bd48dc470c8f1f488864c7ef6ae756d507111e
M.Fathin Halim <halimfathin7@gmail.com>
2025-03-17 06:50:18 +0700

    ehh countdown

--- src/app/preview/[prompt]/page.js
+++ src/app/preview/[prompt]/page.js
@@ -13,7 +13,7 @@ export default function Home() {
   const [typedMessage, setTypedMessage] = useState("");
   const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
   const [loading, setLoading] = useState(true);
-  const [countdown, setCountdown] = useState(5);
+  const [countdown, setCountdown] = useState(3);
   const [showMessage, setShowMessage] = useState(false);
   const [bgm, setBgm] = useState(null);
   const [isReady, setIsReady] = useState(false); // Untuk menampilkan tombol setelah fetch selesai
@@ -150,12 +150,12 @@ export default function Home() {
       return;
 
     const interval = setInterval(() => {
-      setCountdown(5);
+      setCountdown(3);
       setRandomImageIndex(Math.floor(Math.random() * cuteImages.length));
       setCurrentMessageIndex((prev) =>
         prev + 1 < messages.length ? prev + 1 : prev
       );
-    }, 5000);
+    }, 3000);
 
     return () => clearInterval(interval);
   }, [messages, currentMessageIndex, showMessage]);
@@ -171,7 +171,7 @@ export default function Home() {
     let start = performance.now();
     function updateCountdown(timestamp) {
       const elapsed = (timestamp - start) / 1000;
-      const remaining = Math.max(5 - elapsed, 0);
+      const remaining = Math.max(3 - elapsed, 0);
       setCountdown(remaining);
       if (remaining > 0) {
         requestAnimationFrame(updateCountdown);
@@ -187,7 +187,7 @@ export default function Home() {
       if (messages.length === 0 || currentMessageIndex === messages.length - 1)
         return;
 
-      setCountdown(5);
+      setCountdown(3);
       setRandomImageIndex(Math.floor(Math.random() * cuteImages.length));
       setCurrentMessageIndex((prev) =>
         prev + 1 < messages.length ? prev + 1 : prev
@@ -256,7 +256,7 @@ export default function Home() {
             <progress
               type="range"
               min="0"
-              max="5"
+              max="3"
               value={countdown}
               readOnly
               className="w-full w-100 mt-2 transition-all duration-500 ease-in-out"
